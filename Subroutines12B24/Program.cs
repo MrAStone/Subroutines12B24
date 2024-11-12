4014c6a5e2fdf2b7a6399c90c5a9da44fa0c7e4b
Al Stone <astone@heckgrammar.co.uk>
2024-11-12 12:01:56 +0000

    Add project files.

---
+++ Subroutines12B24/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Subroutines12B24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( getANumber());
+            int n = getANumber();
+            outputSomething();
+            outputSomething();
+            outputSomething();
+            outputSomething(); 
+            outputSomething();
+            Console.WriteLine(correctLength("myString"));
+
+            string password = "letMeInNow123";
+            Console.WriteLine(correctLength(password));
+            multiParameter(password, n, true);
+        }
+        static bool correctLength(string input)
+        {
+            if (input.Length >= 12)
+            {
+                return true;
+            }
+            return false;
+            Console.WriteLine("This will never run!");
+        }
+        static void multiParameter(string first, int second, bool third)
+        {
+            Console.WriteLine($"{first} {second} {third}");
+        }
+        static int getANumber()
+        {
+            return 42;
+        }
+        static void outputSomething()
+        {
+            Console.WriteLine("something else");
+        }
+       
+    }
+}
